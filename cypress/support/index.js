2c2e1c9b90240e7fc6f273f0655f751b10fb9c1b
Lin Wang <wonglam@amazon.com>
2026-01-28 15:19:55 +0800

    Add real events
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import "cypress-real-events";
+
 
 const resizeObserverLoopErrRe = /^[^(ResizeObserver loop limit exceeded)]/;
 Cypress.on('uncaught:exception', (err) => {
