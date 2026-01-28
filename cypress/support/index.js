e5a43934eb7b761628bf919d7868d9cf42014515
Lin Wang <wonglam@amazon.com>
2026-01-28 15:10:42 +0800

    Fix uncaught exception
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -3,3 +3,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+
+const resizeObserverLoopErrRe = /^[^(ResizeObserver loop limit exceeded)]/;
+Cypress.on('uncaught:exception', (err) => {
+  /* returning false here prevents Cypress from failing the test */
+  if (resizeObserverLoopErrRe.test(err.message)) {
+    return false;
+  }
+});
